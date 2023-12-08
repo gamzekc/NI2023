01a00fd31436a40805729896a0839159cfe61489
Ludo <ludovic.cheron.Etu@univ-lemans.fr>
2023-12-08 02:22:35 +0100

    Improve creation json and composant card

---
+++ robot-main-site-vitrine-environnement/src/app/public/question-card/question-card.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
+@Component({
+  selector: 'app-question-card',
+  standalone: true,
+  imports: [HttpClientModule],
+  templateUrl: './question-card.component.html',
+  styleUrl: './question-card.component.css'
+})
+export class QuestionCardComponent implements OnInit{
+  Id : any;
+  Theme : any;
+  Description : any;
+  Question : any;
+  card_question: any = [];
+  
+ 
+  private jsonUrl = 'C:\JeuDechec\NI202\robot-main-site-vitrine-environnement\src\assets\Json\question.json';
+
+  constructor(private http: HttpClient) { }
+ ngOnInit(): void {
+    this.http.get(this.jsonUrl).subscribe((data) => {
+      this.card_question = data;
+  });
+  }
+ 
+
+}
