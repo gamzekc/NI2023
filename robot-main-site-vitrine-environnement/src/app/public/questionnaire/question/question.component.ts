6ab5c77c558a114f5312b00edd4ad2a166827ca2
ml-devops44 <contact.mldevops@gmail.com>
2023-12-08 02:48:25 +0100

    questions

--- robot-main-site-vitrine-environnement/src/app/public/questionnaire/question/question.component.ts
+++ robot-main-site-vitrine-environnement/src/app/public/questionnaire/question/question.component.ts
@@ -1,12 +1,58 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-question',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './question.component.html',
   styleUrl: './question.component.css'
 })
 export class QuestionComponent {
+  response1 : boolean = false;
+  response2 : boolean = false;
+  response3 : boolean = false;
+  response4 : boolean = false;
+  multiselect : boolean = false;
+
+  select(id: number){
+    if(!this.multiselect){
+      if(this.response1 || this.response2 || this.response3 || this.response4){
+        if(id == 1 && this.response1){
+          this.response1 = !this.response1;
+        }else{
+          
+          if(id == 2 && this.response2){
+            this.response2 = !this.response2;
+          }else{
+            if(id == 3 && this.response3){
+              this.response3 = !this.response3;
+            }else{
+              if(id == 4 && this.response4){
+                this.response4 = !this.response4;
+              }else{
+
+              }
+            }
+          }
+        }
+      }else{
+        switch(id){
+          case 1 : this.response1 = !this.response1;break;
+          case 2 : this.response2 = !this.response2;break;
+          case 3 : this.response3 = !this.response3;break;
+          case 4 : this.response4 = !this.response4;break;
+        }
+      }
+    }else{
+      switch(id){
+        case 1 : this.response1 = !this.response1;break;
+        case 2 : this.response2 = !this.response2;break;
+        case 3 : this.response3 = !this.response3;break;
+        case 4 : this.response4 = !this.response4;break;
+      }
+    }
+
+  }
 
 }
--- robot-main-site-vitrine-environnement/src/app/public/questionnaire/questionnaire.component.ts
+++ robot-main-site-vitrine-environnement/src/app/public/questionnaire/questionnaire.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { QuestionComponent } from './question/question.component';
 
 @Component({
   selector: 'app-questionnaire',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, FooterComponent, QuestionComponent],
   templateUrl: './questionnaire.component.html',
   styleUrl: './questionnaire.component.css'
 })
